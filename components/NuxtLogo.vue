000a1929982ffcc5ab3759eb60d5c3601d9aad4a
김태훈 <kimtaehoon@smilegate.com>
2022-03-16 09:25:05 +0900

    - init nuxt project - add global css

---
+++ components/NuxtLogo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>ABC</h1>
+    <nuxt-link to="post">Go POst</nuxt-link>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+export default Vue.extend({
+  name: 'NuxtLogo'
+});
+</script>
+<style>
+.nuxt-logo {
+  height: 180px;
+}
+</style>
